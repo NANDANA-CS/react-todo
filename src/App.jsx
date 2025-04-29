86fcc1e9b2ed349a66078cc94e28180552a472d6
NANDANA-CS <csnandana95@gmail.com>
2025-04-29 16:31:05 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,113 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import './App.css'
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       input: "",
-      todos: []
+      todos: [],
+      showModal: false,
+      modalAction: null,
+      modalData: null,
+      editingIndex: null,
+      editInput: "",
     }
-    console.log("constructor created");
+    console.log("constructor created")
   }
 
   componentDidMount() {
-    console.log("Component Mounted");
+    console.log("Component Mounted")
   }
 
   componentDidUpdate() {
     console.log("component updated")
   }
+
   componentWillUnmount() {
-    console.log("component unmounted");
+    console.log("component unmounted")
   }
 
-  addTodo = () => {
+  AddTodo = () => {
     this.setState({
-      todos: [...this.state.todos, this.state.input],
-      input: ''
+      modalAction: 'add',
+      modalData: this.state.input,
+      showModal: true,
     })
   }
 
+  DeleteTodo = (index) => {
+    this.setState({
+      modalAction: 'delete',
+      modalData: index,
+      showModal: true,
+    })
+  }
+
+  confirmAction = () => {
+    const { modalAction, modalData } = this.state;
+    if (modalAction === 'add' && modalData) {
+      this.setState({
+        todos: [...this.state.todos, modalData],
+        input: '',
+        showModal: false,
+        modalAction: null,
+        modalData: null,
+      })
+    } else if (modalAction === 'delete' && modalData !== null) {
+      this.setState({
+        todos: this.state.todos.filter((i, index) => index !== modalData),
+        showModal: false,
+        modalAction: null,
+        modalData: null,
+      })
+    }
+  }
+
+  cancelAction = () => {
+    this.setState({
+      showModal: false,
+      modalAction: null,
+      modalData: null,
+    })
+  }
 
   handleChange = (e) => {
     this.setState({ input: e.target.value })
   }
-
-  deleteTodo = (del) => {
-    this.setState({
-      todos: this.state.todos.filter((e, index) => index !== del
-      )
-    })
-  }
-
   render() {
     return (
       <div className="container">
-        <h1>TODO With REACT</h1>
-        <input type="text" placeholder="Enter Task" value={this.state.input} onChange={this.handleChange} className="taskInp" />
-        <button onClick={this.addTodo}>Add Task</button>
-        <ul>
-          {
-            this.state.todos.map((todo, index) => {
-              return <li key={index}>{todo} <button id="delete" onClick={() => this.deleteTodo(index)}>Delete</button></li>
-            })
-          }
+        <h1>REACT TO-DO LIST</h1>
+        <div className="input-group">
+          <input type="text" placeholder="Enter Task" value={this.state.input} onChange={this.handleChange} className="taskInp" />
+          <button onClick={this.AddTodo}>Add Task </button>
+        </div>
+        <ul>{this.state.todos.map((todo, index) => (
+          <li key={index}>{(
+            <>
+              {todo}
+              <div className="task-buttons">
+                <button id="delete" onClick={() => this.DeleteTodo(index)}>Delete</button>
+              </div>
+            </>
+          )}
+          </li>
+        ))}
         </ul>
+        {
+          this.state.showModal && (
+            <div className="modal-overlay">
+              <div className="modal">
+                <h2>{this.state.modalAction === 'add' ? 'Confirm Add Task' : 'Confirm Delete Task'}</h2>
+                <p>{this.state.modalAction === 'add' ? `Are you sure you want to add "${this.state.modalData}"?` : 'Are you sure you want to delete this task?'}</p>
+                <div className="modal-buttons">
+                  <button onClick={this.cancelAction} className="cancel-btn">Cancel</button>
+                  <button onClick={this.confirmAction} className="confirm-btn" >Confirm </button>
+                </div>
+              </div>
+            </div>
+          )}
       </div>
     )
   }
